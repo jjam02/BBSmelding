d19113382f7096d9bd9920d7f4341c64a4ffcda9
Jonathan Alvarez <jony02@icloud.com>
2025-01-24 17:26:36 -0800

    filter per character works

--- melding_guide/src/App.tsx
+++ melding_guide/src/App.tsx
@@ -6,21 +6,40 @@ import './App.css'
 
 function App() {
 
+  const [filter, setFilter] = useState<string>("");
 
+  function updateFilter(event: React.MouseEvent<HTMLImageElement>): void {
+    const char = Array.from(event.currentTarget.classList)[1];
+    if (filter.includes(char)) {
+      setFilter((prevFilter) => prevFilter.replace(char, ""));
+    } else {
+      setFilter((prevFilter) => prevFilter + char + "\n");
+
+    }
+
+  }
+
+  function clearFilter(): void {
+    setFilter((prevFilter) => "");
+  }
 
   return (
     <>
       <div>
 
-        <img src={terra} className="char terra" alt="React logo" />
-        <img src={ventus} className="char ventus" alt="React logo" />
-        <img src={aqua} className="char aqua" alt="React logo" />
+        <img onClick={updateFilter} src={terra} className="char Terra" alt="React logo" />
+        <img onClick={updateFilter} src={ventus} className="char Ventus" alt="React logo" />
+        <img onClick={updateFilter} src={aqua} className="char Aqua" alt="React logo" />
       </div>
+      <button onClick={clearFilter}>Clear Filter</button>
       <h1>Melding BBS</h1>
       <div className="card">
 
         <p>
           Melding guide for Kingdom Hearts: Birth By Sleep Final Mix
+        </p>
+        <p>
+          current filter: {filter}
         </p>
       </div>
       <p className="read-the-docs">
